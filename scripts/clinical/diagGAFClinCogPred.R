c0424e5c1a988e5465107777f9422f3f926deb43
Ellyn Butler <ellynrbutler@gmail.com>
2020-12-08 22:23:09 -0500

    prepare for abstract

---
+++ scripts/clinical/diagGAFClinCogPred.R
@@ -0,0 +1,85 @@
+### This script runs models of baseline factor scores and other clinical dimensions
+### predicting final PS status
+###
+### Ellyn Butler
+### December 1, 2020 - December 2, 2020
+
+library('dplyr') # Version 1.0.2
+library('sjPlot') # Version 2.8.4
+library('reshape2') # Version 1.4.4
+library('fastDummies') # Version 1.6.1
+
+clin_df <- read.csv('~/Documents/pncLongitudinalPsychosis/data/clinical/pnc_longitudinal_diagnosis_n752_202007.csv')
+demo_df <- read.csv('~/Documents/pncLongitudinalPsychosis/data/demographics/baseline/n1601_demographics_go1_20161212.csv')
+#factor_df <- read.csv('~/Documents/predLongLabels/data/n9498_goassess_itemwise_corrtraits_scores_20161219.csv')
+ratdiag_df <- read.csv('~/Documents/pncLongitudinalPsychosis/data/clinical/n9498_goassess_psych_summary_vars_20131014.csv')
+cog_df <- read.csv('~/Documents/pncLongitudinalPsychosis/data/cognitive/cnb_quickFA_impute_2020-10-20.csv')
+
+
+cog_df <- cog_df[cog_df$Timepoint == 1, ]
+names(cog_df)[names(cog_df) == 'EFF_Soln4_MR1'] <- 'SocCog_EFF'
+names(cog_df)[names(cog_df) == 'EFF_Soln4_MR2'] <- 'Exec_EFF'
+names(cog_df)[names(cog_df) == 'EFF_Soln4_MR3'] <- 'Mem_EFF'
+names(cog_df)[names(cog_df) == 'EFF_Soln4_MR4'] <- 'CompCog_EFF'
+cog_df <- cog_df[, c('bblid', 'SocCog_EFF', 'Exec_EFF', 'Mem_EFF', 'CompCog_EFF')]
+
+colsforplot <- grep('smry', names(ratdiag_df), value=TRUE)
+colsforplot <- colsforplot[!(colsforplot %in% c('smry_prime_pos1', 'smry_prime_tot',
+  'smry_prime_pos2', 'smry_psych_overall_rtg', grep('hal', colsforplot, value=TRUE),
+  grep('del', colsforplot, value=TRUE)))]
+
+ratdiag_df$ratings_sum <- scale(rowSums(ratdiag_df[, colsforplot]))
+
+
+final_df <- merge(clin_df, demo_df, by='bblid')
+final_df <- merge(final_df, ratdiag_df, by='bblid')
+final_df <- merge(final_df, cog_df, by='bblid')
+names(final_df)[names(final_df) == 't1'] <- 'first_diagnosis'
+names(final_df)[names(final_df) == 'tfinal2'] <- 'last_diagnosis'
+final_df$Female <- recode(final_df$sex, `2`=1, `1`=0)
+final_df$White <- recode(final_df$race, `1`=1, .default=0)
+final_df$first_diagnosis <- recode(final_df$first_diagnosis, 'other'='OP')
+final_df$last_diagnosis <- recode(final_df$last_diagnosis, 'other'='OP')
+final_df$t1_tfinal <- recode(final_df$t1_tfinal, 'other_other'='OP_OP',
+  'other_TD'='OP_TD', 'other_PS'='OP_PS', 'TD_other'='TD_OP', 'PS_other'='PS_OP')
+final_df <- within(final_df, t1_tfinal <- relevel(t1_tfinal, ref='TD_TD'))
+final_df <- within(final_df, first_diagnosis <- relevel(first_diagnosis, ref='TD'))
+
+final_df$PS_final <- recode(final_df$tfinal, 'other'='No', 'PS'='Yes',
+  'psy'='Yes', 'TD'='No')
+
+final_df$sex <- recode(final_df$sex, `2`='Female', `1`='Male')
+final_df$sex <- as.factor(final_df$sex)
+final_df <- within(final_df, sex <- relevel(sex, ref='Male'))
+
+#final_df <- merge(final_df, factor_df)
+#final_df[, grep('corr', names(factor_df), value=TRUE)] <- sapply(final_df[, grep('corr', names(factor_df), value=TRUE)], scale)
+
+###############################################################################
+
+
+# Sum of ratings
+mod0_rat <- glm(PS_final ~ first_diagnosis, family='binomial', data=final_df)
+mod1_rat <- glm(PS_final ~ ratings_sum, family='binomial', data=final_df)
+mod2_rat <- glm(PS_final ~ CompCog_EFF, family='binomial', data=final_df)
+mod3_rat <- glm(PS_final ~ first_diagnosis + ratings_sum + CompCog_EFF,
+  family='binomial', data=final_df)
+mod4_rat <- glm(PS_final ~ first_diagnosis + ratings_sum + CompCog_EFF + first_diagnosis:ratings_sum + first_diagnosis:CompCog_EFF + ratings_sum:CompCog_EFF,
+  family='binomial', data=final_df)
+mod5_rat <- glm(PS_final ~ first_diagnosis*ratings_sum*CompCog_EFF,
+  family='binomial', data=final_df)
+
+tab_model(mod0_rat, mod1_rat, mod2_rat, mod3_rat, mod4_rat, mod5_rat)
+
+# Sum of ratings, controlling for age
+mod0_rat <- glm(PS_final ~ ageAtClinicalAssess1 + first_diagnosis, family='binomial', data=final_df)
+mod1_rat <- glm(PS_final ~ ageAtClinicalAssess1 + ratings_sum, family='binomial', data=final_df)
+mod2_rat <- glm(PS_final ~ ageAtClinicalAssess1 + CompCog_EFF, family='binomial', data=final_df)
+mod3_rat <- glm(PS_final ~ ageAtClinicalAssess1 + first_diagnosis + ratings_sum + CompCog_EFF,
+  family='binomial', data=final_df)
+mod4_rat <- glm(PS_final ~ ageAtClinicalAssess1 + first_diagnosis + ratings_sum + CompCog_EFF + first_diagnosis:ratings_sum + first_diagnosis:CompCog_EFF + ratings_sum:CompCog_EFF,
+  family='binomial', data=final_df)
+mod5_rat <- glm(PS_final ~ ageAtClinicalAssess1 + first_diagnosis*ratings_sum*CompCog_EFF,
+  family='binomial', data=final_df)
+
+tab_model(mod0_rat, mod1_rat, mod2_rat, mod3_rat, mod4_rat, mod5_rat)
--- scripts/clinical/plot_fits_diag_sips.R
+++ scripts/clinical/plot_fits_diag_sips.R
@@ -18,16 +18,17 @@ library('sjPlot')
 
 clin_df <- read.csv('~/Documents/pncLongitudinalPsychosis/data/clinical/clean_sips.csv')
 diag_df <- read.csv('~/Documents/pncLongitudinalPsychosis/data/clinical/pnc_longitudinal_diagnosis_n752_202007.csv')
-diag_df$t1_tfinal <- recode(diag_df$t1_tfinal, 'TD_TD'='TD_TD', 'TD_other'='TD_OP',
-  'TD_PS'='TD_PS', 'other_TD'='OP_TD', 'other_other'='OP_OP', 'other_PS'='OP_PS',
-  'PS_TD'='PS_TD', 'PS_other'='PS_OP', 'PS_PS'='PS_PS')
+diag_df$t1_tfinal <- recode(diag_df$t1_tfinal, 'TD_TD'='TD-TD', 'TD_other'='TD-OP',
+  'TD_PS'='TD-PS', 'other_TD'='OP-TD', 'other_other'='OP-OP', 'other_PS'='OP-PS',
+  'PS_TD'='PS-TD', 'PS_other'='PS-OP', 'PS_PS'='PS-PS')
 
 final_df <- merge(clin_df, diag_df, by='bblid')
 
-final_df$t1_tfinal <- relevel(final_df$t1_tfinal, 'TD_TD')
+final_df$t1_tfinal <- factor(final_df$t1_tfinal)
+final_df$t1_tfinal <- relevel(final_df$t1_tfinal, 'TD-TD')
 
-final_df$oT1_Tfinal <- ordered(final_df$t1_tfinal, c('TD_TD', 'OP_OP', 'OP_PS',
-  'OP_TD', 'PS_OP', 'PS_PS', 'PS_TD', 'TD_OP', 'TD_PS'))
+final_df$oT1_Tfinal <- ordered(final_df$t1_tfinal, c('TD-TD', 'OP-OP', 'OP-PS',
+  'OP-TD', 'PS-OP', 'PS-PS', 'PS-TD', 'TD-OP', 'TD-PS'))
 
 names(final_df)[names(final_df) == 'gaf_c'] <- 'GAF_Current'
 names(final_df)[names(final_df) == 'gaf_h'] <- 'GAF_Highest'
@@ -44,7 +45,7 @@ getUpperLowerCI <- function(i) {
 
 plotcols <- c('Positive', 'Negative', 'Disorganized', 'General', 'GAF_Current', 'GAF_Highest')
 
-diags <- as.character(unique(final_df$t1_tfinal)[!(unique(final_df$t1_tfinal) %in% c('TD_TD', 'PS_PS'))])
+diags <- as.character(unique(final_df$t1_tfinal)[!(unique(final_df$t1_tfinal) %in% c('TD-TD', 'PS-PS'))])
 
 for (score in plotcols) {
   clin_score_df <- final_df
@@ -73,33 +74,33 @@ for (score in plotcols) {
   clin_score_df$predgamm <- predict(mod1b$gam)
 
   for (group in diags) {
-    subtit <- paste0('Sessions: TD-TD=', nrow(clin_score_df[clin_score_df$t1_tfinal == 'TD_TD',]),
+    subtit <- paste0('Sessions: TD-TD=', nrow(clin_score_df[clin_score_df$t1_tfinal == 'TD-TD',]),
       ', ', group, '=', nrow(clin_score_df[clin_score_df$t1_tfinal == group,]),
-      ', PS-PS=', nrow(clin_score_df[clin_score_df$t1_tfinal == 'PS_PS',]))
-    group_df <- clin_score_df[clin_score_df$t1_tfinal %in% c('TD_TD', group, 'PS_PS'), ]
-    group_df$t1_tfinal <- ordered(group_df$t1_tfinal, c('TD_TD', group, 'PS_PS'))
+      ', PS-PS=', nrow(clin_score_df[clin_score_df$t1_tfinal == 'PS-PS',]))
+    group_df <- clin_score_df[clin_score_df$t1_tfinal %in% c('TD-TD', group, 'PS-PS'), ]
+    group_df$t1_tfinal <- ordered(group_df$t1_tfinal, c('TD-TD', group, 'PS-PS'))
     clin_plot <- ggplot(group_df,
         aes(x=Age, y=score, color=t1_tfinal)) + theme_linedraw() +
       scale_color_manual(values=c('green3', 'goldenrod2', 'red')) +
       theme(legend.position = 'bottom', plot.title=element_text(size=14, face="bold"),
         plot.subtitle=element_text(size=8)) +
-      labs(title=score, subtitle=subtit) + geom_line(aes(y=predgamm), size=1) +
-      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'TD_TD',], aes(y=LCI),
+      labs(title=score, subtitle=subtit, color='Group') + geom_line(aes(y=predgamm), size=1) +
+      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'TD-TD',], aes(y=LCI),
         size=.7, linetype=2, color='gray20') +
-      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'TD_TD',], aes(y=UCI),
+      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'TD-TD',], aes(y=UCI),
         size=.7, linetype=2, color='gray20') +
       geom_line(data=clin_score_df[clin_score_df$t1_tfinal == group,], aes(y=LCI),
         size=.7, linetype=2, color='gray40') +
       geom_line(data=clin_score_df[clin_score_df$t1_tfinal == group,], aes(y=UCI),
         size=.7, linetype=2, color='gray40') +
-      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'PS_PS',], aes(y=LCI),
+      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'PS-PS',], aes(y=LCI),
         size=.7, linetype=2, color='gray60') +
-      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'PS_PS',], aes(y=UCI),
+      geom_line(data=clin_score_df[clin_score_df$t1_tfinal == 'PS-PS',], aes(y=UCI),
         size=.7, linetype=2, color='gray60')
 
     assign(paste0(score, '_', group, '_plot'), clin_plot)
 
-    pdf(file=paste0('~/Documents/pncLongitudinalPsychosis/plots/clin_', score, '_', group, '.pdf'), width=4, height=4)
+    pdf(file=paste0('~/Documents/pncLongitudinalPsychosis/plots/clin_', score, '_', group, '_clean.pdf'), width=4, height=4)
     print(clin_plot)
     dev.off()
   }
